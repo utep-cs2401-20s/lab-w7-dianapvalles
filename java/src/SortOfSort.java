e10507b1561aeb90e5224005a54d6cbec1a74280
dianapvalles <dianapaolavalles@gmail.com>
2020-03-03 09:43:27 -0700

    Corrected sortOfSort method

--- java/src/SortOfSort.java
+++ java/src/SortOfSort.java
@@ -1,33 +1,43 @@
 public class SortOfSort {
-
-    public static void sortOfSort(int[] nums){
+    public static void sortOfSort(int[] nums) {
+        int lowestIndex= 0;
+        int highestIndex = nums.length-1;
+        int max;
+        int indexCurr;
         int count = 0;
-        int indexSmallest = 0;
-        int max = Integer.MIN_VALUE;
+        int temp;
 
-        for(int i  = nums.length-1; i >= 0; i++){
-
-            for(int j = 0; j < nums.length-1; j++){
-                if(count >= 2){
-                    count = 0;
-                }
-
-                if(nums[i] > max){
-                    max = nums[i];
-                }
-            }
-
-            if(count < 2){
-                nums[i] = max;
-                count++;
-            }
-            else{
-                nums[indexSmallest] = max;
-                indexSmallest++;
-            }
-
+        for(int i = 0; i < nums.length - 1 ; i++){
             max = Integer.MIN_VALUE;
+            indexCurr = i;
 
+            for(int j = lowestIndex ; j <= highestIndex; j++){
+                if(nums[j] > max){
+                    max = nums[j];
+                    indexCurr = j;
+                }
+            }
+
+            if(lowestIndex < highestIndex) {
+                if (count < 2) {
+                    temp = nums[highestIndex];
+                    nums[highestIndex] = max;
+                    nums[indexCurr] = temp;
+                    count++;
+                    highestIndex--;
+                }
+                else if (count < 4) {
+                    temp = nums[lowestIndex];
+                    nums[lowestIndex] = max;
+                    nums[indexCurr] = temp;
+                    count++;
+                    lowestIndex++;
+                }
+            }
+
+            if(count == 4){
+                count = 0;
+            }
         }
     }
 }
